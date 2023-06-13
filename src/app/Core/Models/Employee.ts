167291ba75c916268be74adf1cbb33b59862e396
Dina Gezaf <106817687+DinaGezaf@users.noreply.github.com>
2023-06-13 21:17:35 +0300

    Sales Representative

---
+++ src/app/Core/Models/Employee.ts
@@ -0,0 +1,11 @@
+export class Employee {
+  constructor(
+    public Employee_Id: string,
+    public Name: string,
+    public Email: string,
+    public PhoneNo: string,
+    public Branch: string,
+    public Privellge: string,
+    public IsActive: boolean
+  ) {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './Shared/SharedModule/shared.module';
@@ -24,6 +25,17 @@ import { SalesRepresentativeModule } from './Components/SalesRepresentative/sale
     BrowserAnimationsModule,
     SalesRepresentativeModule,
   ],
+=======
+import { AddComponent } from './components/Employee/add/add.component';
+import { EditComponent } from './components/Employee/edit/edit.component';
+import { DisplayComponent } from './components/Employee/display/display.component';
+import { DeleteComponent } from './components/Privellge/delete/delete.component';
+import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
+
+@NgModule({
+  declarations: [AppComponent, AddComponent, EditComponent, DisplayComponent, DeleteComponent, SidemenuComponent],
+  imports: [BrowserModule, AppRoutingModule],
+>>>>>>> 6e394095691dac357cc2baa6e8489b2f7d785c20
   providers: [],
   bootstrap: [AppComponent],
 })
