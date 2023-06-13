6e394095691dac357cc2baa6e8489b2f7d785c20
Simonmohamed <simonmohamed9@gmail.com>
2023-06-13 17:39:11 +0200

    display employees

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './components/Employee/add/add.component';
+import { EditComponent } from './components/Employee/edit/edit.component';
+import { DisplayComponent } from './components/Employee/display/display.component';
+import { DeleteComponent } from './components/Privellge/delete/delete.component';
+import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AddComponent, EditComponent, DisplayComponent, DeleteComponent, SidemenuComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
